064b679fe69ff827ee3d97764f8e80afa71a0a4d
Zameer Ansari <xameeramir@users.noreply.github.com>
2017-05-09 20:04:18 +0530

    Update startPoorMansClusterClient.js

--- startPoorMansClusterClient.js
+++ startPoorMansClusterClient.js
@@ -2,9 +2,9 @@ var poorMansClusterClient = require('redis-cluster').poorMansClusterClient;
 var assert = require('assert');
  
 var cluster = [
-  {name: 'redis01', link: 'bots-elasticache-0001-003.nyonfm.0001.usw2.cache.amazonaws.com:6379', slots: [   0, 5462], options: {max_attempts: 5}},
-  {name: 'redis02', link: 'bots-elasticache-0001-002.nyonfm.0001.usw2.cache.amazonaws.com:7379', slots: [5463, 12742], options: {max_attempts: 5}},
-  {name: 'redis03', link: 'bots-elasticache-0001-001.nyonfm.0001.usw2.cache.amazonaws.com:8379', slots: [12743, 16384], options: {max_attempts: 5}}
+  {name: 'redis01', link: 'AMAZON ELASTICACHE CLUSTER INDIVIDUAL INSTANCE URL:6379', slots: [   0, 5462], options: {max_attempts: 5}},
+  {name: 'redis02', link: 'AMAZON ELASTICACHE CLUSTER INDIVIDUAL INSTANCE URL:7379', slots: [5463, 12742], options: {max_attempts: 5}},
+  {name: 'redis03', link: 'AMAZON ELASTICACHE CLUSTER INDIVIDUAL INSTANCE URL:8379', slots: [12743, 16384], options: {max_attempts: 5}}
 ];
  
 var r = poorMansClusterClient(cluster);
